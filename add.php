84c8ac71ac141b4e225801566525f10e3dbbd8a3
kerim <kerim.qafarli02@gmail.com>
2022-04-24 14:43:24 +0400

    main

---
+++ add.php
@@ -0,0 +1,16 @@
+<?php
+   $task = $_post['task'];
+   if($task == '') {
+     echo 'введите само задание';
+     exit();
+   }
+
+   $dsn = 'mysql:host=localhost;dbname=kera_addressbook';
+   $pdo = new PDO($dsn, 'root', 'root');
+
+   $sql = 'INSERT INTO tasks(task) VALUES(:task)';
+   $query = $pdo->prepare($sql);
+   $query->execute(['task => $task']);
+
+   header('Location: /');
+?>
