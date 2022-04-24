a49aa4017ba0400e33cbd99b741155c564bb4cc2
kerim <kerim.qafarli02@gmail.com>
2022-04-24 13:06:03 +0400

    main

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <title>Список контактов</title>
 </head>
 <body>
-    контакты
+    <h1>контакты</h1>
 </body>
 </html>
