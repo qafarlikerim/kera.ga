d135ccb652a66393d62d1b0ade14830185ad13b0
kerim <kerim.qafarli02@gmail.com>
2022-04-24 13:44:35 +0400

    main

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Список контактов</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <title>Список контактов</title>
 </head>
 <body>
   <div class="container">
-    <h1>контакты</h1>
+    <h1>список контактов</h1>
     <form action="/add.php" method="post">
         <input type="text" name="task id="task placeholder="нужно сделать,," class="from-control">
         <butoon type="submit" name="sendTask" class="btn btn-succes">отправить</button>
