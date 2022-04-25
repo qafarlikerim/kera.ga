ee017611b8948e2187c32a49e2654bcf82a200c4
kerim <kerim.qafarli02@gmail.com>
2022-04-25 10:55:58 +0400

    main

--- add.php
+++ add.php
@@ -5,7 +5,7 @@
     exit();
   }
 
-  $dsn = 'my_db:host=localhost;dbname=kera_addressbook';
+  $dsn = 'mysqli:host=localhost;dbname=kera_addressbook';
   $pdo = new PDO($dsn, 'kera_kera', '5813910K');
 
   $sql = 'INSERT INTO tasks(task) VALUES(:task)';
