0bde4a6f0d42170b897ac67edb9d5cc1082649d6
kerim <kerim.qafarli02@gmail.com>
2022-04-25 12:02:15 +0400

    main

--- index.php
+++ index.php
@@ -5,9 +5,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Список контактов</title>
-  <link rel="stylesheet" href="css/style.css">
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
   <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"> -->
 </head>
+
+<style>
+  body {background-color: #EFF0F3}
+
+.container {
+  margin-top: 50px;
+  width: 25%;
+}
+
+form {
+  background: #2FCC9A;
+  border: 2px solid #228e6c;
+  border-radius: 10px;
+  padding: 20px 10px;
+}
+
+#task {
+  width: 60%;
+  float: left;
+  margin-right: 5%;
+}</style>
+
 <body>
   <div class="container">
     <h1>Список контактов</h1>
