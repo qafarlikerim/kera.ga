2fb1433deaaada97494468087613c4f325c2e379
kerim <kerim.qafarli02@gmail.com>
2022-04-27 19:29:23 +0400

    Update add.php

--- add.php
+++ add.php
@@ -1,4 +1,18 @@
 <?php
+  $name = $_POST['name'];
+  if($name == '') {
+    echo 'Введите имя контакта';
+    exit();
+  }
+
+  require 'configDB.php';
+ 
+  $sql = 'INSERT INTO people(name) ALUES(:name)'; 
+  $query = $pdo->prepare($sql);
+  $query->execute(['name' => $name]);
+
+  header('Location: /');
+
   $number = $_POST['number'];
   if($number == '') {
     echo 'Введите номер контакта';
@@ -10,20 +24,6 @@
   $sql = 'INSERT INTO people(number) VALUES(:number)'; 
   $query = $pdo->prepare($sql);
   $query->execute(['number' => $number]);
-
-  header('Location: /');
-
-  $name = $_POST['name'];
-  if($name == '') {
-    echo 'Введите имя контакта';
-    exit();
-  }
-
-  require 'configDB.php';
-  
-  $sql = 'INSERT INTO people(name) ALUES(:name)'; 
-  $query = $pdo->prepare($sql);
-  $query->execute(['name' => $name]);
 
   header('Location: /');
 
