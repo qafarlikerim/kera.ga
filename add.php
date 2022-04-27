4b9a01910f11a3ef53af94c3b25c1f1d51ee477d
kerim <kerim.qafarli02@gmail.com>
2022-04-27 19:23:08 +0400

    Update add.php

--- add.php
+++ add.php
@@ -13,4 +13,17 @@
 
   header('Location: /');
 
+  $name = $_POST['name'];
+  if($name == '') {
+    echo 'Введите имя контакта';
+    exit();
+  }
+
+  require 'configDB.php';
+  $sql = 'INSERT INTO people(name) ALUES(:name)'; 
+  $query = $pdo->prepare($sql);
+  $query->execute(['name' => $name]);
+
+  header('Location: /');
+
 ?>
