6385c11b691ba2381f8a1eeb012d7b93a127c4c6
kerim <kerim.qafarli02@gmail.com>
2022-05-10 11:22:28 +0400

    main

--- add.php
+++ add.php
@@ -13,19 +13,3 @@
 
   header('Location: /');
 ?>
-<?php
-  $number = $_POST['number'];
-  if($number == '') {
-    echo 'Введите номер контакта';
-    exit();
-  }
-
-  require 'configDB.php';
-
-  $sql = 'INSERT INTO numbers(number) VALUES(:number)'; 
-  $query = $pdo->prepare($sql);
-  $query->execute(['number' => $number]);
-
-  header('Location: /');
-
-?>
