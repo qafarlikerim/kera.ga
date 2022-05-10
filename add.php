e2aad69396408e79b458e2eaed1df7f3880af49b
kerim <kerim.qafarli02@gmail.com>
2022-05-10 11:57:19 +0400

    main

--- add.php
+++ add.php
@@ -1,18 +1,4 @@
 <?php
-  $name = $_POST['name'];
-  if($name == '') {
-    echo 'Введите имя контакта...';
-    exit();
-  }
-
-  require 'configDB.php';
- 
-  $sql = 'INSERT INTO people(name) VALUES(:name)'; 
-  $query = $pdo->prepare($sql);
-  $query->execute(['name' => $name]);
-
-  header('Location: /');
-
   $number = $_POST['number'];
   if($number == '') {
     echo 'Введите номер контакта...';
@@ -24,6 +10,20 @@
   $sql = 'INSERT INTO numbers(number) VALUES(:number)'; 
   $query = $pdo->prepare($sql);
   $query->execute(['number' => $number]);
+
+  header('Location: /');
+
+  $name = $_POST['name'];
+  if($name == '') {
+    echo 'Введите имя контакта...';
+    exit();
+  }
+
+  require 'configDB.php';
+ 
+  $sql = 'INSERT INTO people(name) VALUES(:name)'; 
+  $query = $pdo->prepare($sql);
+  $query->execute(['name' => $name]);
 
   header('Location: /');
 
