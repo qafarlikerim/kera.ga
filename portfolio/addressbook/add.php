d73ce5747ab572d701d371adb921b6f1076a5c92
kerim <kerim.qafarli02@gmail.com>
2022-05-18 12:59:21 +0400

    main

--- portfolio/addressbook/add.php
+++ portfolio/addressbook/add.php
@@ -1,4 +1,18 @@
 <?php
+  $number = $_POST['number'];
+  if($number == '') {
+    echo 'Введите номер контакта...';
+    exit();
+  }
+
+  require 'configDB.php';
+
+  $sql = 'INSERT INTO numbers(number) VALUES(:number)';
+  $query = $pdo->prepare($sql);
+  $query->execute(['number' => $number]);
+
+  header('Location: /');
+
   $name = $_POST['name'];
   if($name == '') {
     echo 'Введите имя контакта...';
