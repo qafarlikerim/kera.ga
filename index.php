f3632127302dbdf13b2d5875b8aae60bd71c6204
kerim <kerim.qafarli02@gmail.com>
2022-05-22 14:59:42 +0400

    main

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 </table>
   <div class="container">
     <h1>Список контактов</h1>
-    <form action="/add2.php" from action="/add.php" method="post">
+    <form action="/add2.php, /add.php" method="post">
       <input type="text" name="name" id="name" placeholder="Введите имя..." class="form-control">
       <input type="int" name="number" id="number" placeholder="Введите номер..." class="form-control">
       <button type="submit" name="sendTask" class="btn btn-success">Отправить</button>
