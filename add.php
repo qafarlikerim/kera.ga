25ef52d50b7ae7c77ee606485bc6dd80937dbaa0
kerim <kerim.qafarli02@gmail.com>
2022-05-22 15:41:00 +0400

    main

--- add.php
+++ add.php
@@ -1,11 +1,9 @@
 <title>Ощибка...</title>
 <a href="index.php"><img src="https://cdn.create.vista.com/api/media/medium/470867402/stock-vector-back-left-arrow-square-button?token=" title="вернуться назад..." width="4%"></a>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <?php
   $number = $_POST['number'];
   if($number == '') {
-    // echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
-    echo($rez['text']);
+    echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
     echo 'Введите номер контакта...';
     exit();
   }
