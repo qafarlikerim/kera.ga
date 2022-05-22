1795c8bae4746b75b90bd88904d646bf62c620b5
kerim <kerim.qafarli02@gmail.com>
2022-05-22 16:22:54 +0400

    main

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <div>Biz yeni sayta köçdük --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></div>
   <div>Мы переехали на новый сайт --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></div>
   <div>We moved to a new site --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></div>
-  <table width="50%" align="center" border="1">
+  <table align="center" border="1">
     <br>
     <tr>
         <th>№</th>
