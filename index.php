80af997607375bba405e3a44138aa9c98fd01fb8
kerim <kerim.qafarli02@gmail.com>
2022-05-22 19:40:59 +0400

    Update index.php

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
   <title>Список контактов</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<style>
-  h2 .red-text {
-    color: red;
-  }
+  <style>
+table, th, td {
+  border: 5px solid;
+}
 </style>
 </head>
 <body>
-  <h2 class="red-text" color=red;>Biz yeni sayta köçdük --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
+  <h2 class="red-text" style="color: red;">Biz yeni sayta köçdük --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
   <h2 align="center" >Мы переехали на новый сайт --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
   <h2 align="center" >We moved to a new site --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
 
