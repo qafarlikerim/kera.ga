54385eac9e3671171597963ec507f162bcc92d2e
kerim <kerim.qafarli02@gmail.com>
2022-05-22 15:02:08 +0400

    main

--- add.php
+++ add.php
@@ -12,4 +12,18 @@
   $query->execute(['number' => $number]);
 
   header('Location: /');
+  
+  $name = $_POST['name'];
+  if($name == '') {
+    echo 'Введите имя контакта...';
+    exit();
+  }
+
+  require 'configDB.php';
+ 
+  $sql = 'INSERT INTO people(name) VALUES(:name)';
+  $query = $pdo->prepare($sql);
+  $query->execute(['name' => $name]);
+
+  header('Location: /');
 ?>
