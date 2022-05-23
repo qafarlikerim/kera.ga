007812364c1a4adc652d49fb36a05a9379b2e14a
kerim <kerim.qafarli02@gmail.com>
2022-05-23 19:00:54 +0400

    main

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Список контактов</title>
-  <link rel="icon" href="/dart.ico" type="image/x-icon">
+  <link rel="icon" href="/address_book.ico" type="image/x-icon">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
   <style>
