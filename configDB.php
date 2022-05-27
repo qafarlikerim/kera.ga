05a9e8b2c4dccbc30369e5cdacc747a98fa92ae3
kerim <kerim.qafarli02@gmail.com>
2022-05-27 10:32:37 +0400

    main

--- configDB.php
+++ configDB.php
@@ -1,5 +1,9 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=kera_addressbook';
-    $pdo = new PDO($dsn, 'kera_kera', '5813910K');
+
+$pdo = new mysqli("localhost", "kera_kera", "5813910K", "kera_addressbook");
+
+echo $pdo->host_info . "\n";
+
+
+    // $pdo = new PDO($dsn, 'kera_kera', '5813910K');
 ?> 
-  
