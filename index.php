68b77e5e633f43497c93f0c27bc0cbd978e87646
kerim <kerim.qafarli02@gmail.com>
2022-05-31 17:20:33 +0400

    main

--- index.php
+++ index.php
@@ -22,49 +22,8 @@
         background: #b5b5b5;
     }
 </style>
-<style>
-  hr.new5 {
-    border: 3px solid red;
-    border-radius: 5px;
-  }
-</style>
-<style>
-.button {
-  display: inline-block;
-  padding: 15px 20px;
-  font-size: 8px;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  outline: none;
-  color: #fff;
-  background-color: #4CAF50;
-  border: none;
-  border-radius: 15px;
-  box-shadow: 0 9px #999;
-}
-}
-
-.button:hover {background-color: #3e8e41}
-
-.button:active {
-  background-color: #3e8e41;
-  box-shadow: 0 5px #666;
-  transform: translateY(4px);
-}
-
-.button2 {background-color: #008CBA;} /* Blue */
-.button3 {background-color: #f44336;} /* Red */ 
-</style>
   </head>
   <body>
-    <h2 class="red-text" style="color: red;" align="center">Biz yeni sayta köçdük --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
-    <h2 class="red-text" style="color: red;" align="center" >Мы переехали на новый сайт --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
-    <h2 class="red-text" style="color: red;" align="center" >We moved to a new site --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
-  
-    <hr class="new5">
-    
-    <br>
     <table>
         <tr>
             <th>ID</th>
@@ -87,18 +46,6 @@
                 <?php
             }
         ?>
-    </table>       
-    <!-- <div class="container">
-      <h1>Список контактов</h1>
-      <form action="/add.php" method="post">
-        <input type="text" name="name" id="name" placeholder="Введите имя..." class="form-control">
-        <br>
-        <input type="int" name="number" id="number" placeholder="Введите номер..." class="form-control">
-        <br>
-        <input type="date" name="date" id="date" placeholder="Введите дату..." class="form-control">
-        <br>
-        <button type="submit" name="sendTask" class="button button2">+</button>&nbsp;<button type="reset" name="sendTask" class="button button3">-</button> -->
-      </form>  
-    </div>
+    </table>  
   </body>
 </html>
