025d21ed91267e5f0a9b436a7d17655eb7142560
kerim <kerim.qafarli02@gmail.com>
2022-05-31 17:22:48 +0400

    maim

--- index.php
+++ index.php
@@ -8,13 +8,7 @@ require_once 'connect.php';
 <html lang="ru">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Список контактов</title>
-    <link rel="icon" href="/address_book.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-
   </head>
 <style>
     th, td {
