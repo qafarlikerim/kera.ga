f129f9e0e28510844e1323584614aa5f67a6edb6
kerim <kerim.qafarli02@gmail.com>
2022-05-31 17:31:28 +0400

    main

--- configDB.php
+++ configDB.php
@@ -1,13 +1,10 @@
 <?php
-    // $dsn = 'mysql:host=localhost;dbname=kera_addressbook';
-    // $pdo = new PDO($dsn, 'kera_kera', '5813910K');
 
 mysqli_report(MYSQLI_REPORT_OFF);
 
 $connect = mysqli_connect('localhost', 'kera_kera', '5813910K', 'kera_addressbook');
 
 if (!$connect) {
-    echo 'Error connect to database!';
+    die('Error connect to database!');
 }
-
 ?> 
--- index.php
+++ index.php
@@ -1,27 +1,19 @@
+<?php
+
+require_once 'connect.php';
+
+?>
+
 <!DOCTYPE html>
-<html lang="ru">
-  <head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Список контактов</title>
+    <title>Products</title>
     <link rel="icon" href="/address_book.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<style>
-    th, td {
-        padding: 10px;
-    }
-
-    th {
-        background: #606060;
-        color: #fff;
-    }
-
-    td {
-        background: #b5b5b5;
-    }
-</style>
 <style>
   hr.new5 {
     border: 3px solid red;
@@ -56,8 +48,22 @@
 .button2 {background-color: #008CBA;} /* Blue */
 .button3 {background-color: #f44336;} /* Red */ 
 </style>
-  </head>
-  <body>
+</head>
+<style>
+    th, td {
+        padding: 10px;
+    }
+
+    th {
+        background: #606060;
+        color: #fff;
+    }
+
+    td {
+        background: #b5b5b5;
+    }
+</style>
+<body>
     <h2 class="red-text" style="color: red;" align="center">Biz yeni sayta köçdük --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
     <h2 class="red-text" style="color: red;" align="center" >Мы переехали на новый сайт --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
     <h2 class="red-text" style="color: red;" align="center" >We moved to a new site --><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
@@ -65,11 +71,28 @@
     <hr class="new5">
     
     <br>
-    <table width="50%"  align="center">
-      <tr>
-          <th>№</th>
-          <th>Name</th>
-      </tr>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Price</th>
+        </tr>
+
+        <?php
+            $products = mysqli_query($connect, query:"SELECT * FROM `products`");
+            $products = mysqli_fetch_all($products);
+            foreach ($products as $products) {
+                ?>
+                    <tr>
+                        <td><?= $products[0] ?></td>
+                        <td><?= $products[1] ?></td>
+                        <td><?= $products[3] ?></td>
+                        <td><?= $products[2] ?></td>
+                    </tr>
+                <?php
+            }
+        ?>
     </table>
     <div class="container">
       <h1>Список контактов</h1>
@@ -81,30 +104,7 @@
         <input type="date" name="date" id="date" placeholder="Введите дату..." class="form-control">
         <br>
         <button type="submit" name="sendTask" class="button button2">+</button>&nbsp;<button type="reset" name="sendTask" class="button button3">-</button>
-      </form>
-  
-      <?php
-        require_once 'configDB.php';
-  
-        echo '<ul>';
-        $query = $pdo->query('SELECT * FROM `people` ORDER BY `id` DESC');
-        while($row = $query->fetch(PDO::FETCH_OBJ)) {
-         echo '<li><b>'.$row->name.'</b></li>';
-        }
-
-        $query = $pdo->query('SELECT * FROM `numbers` ORDER BY `id` DESC');
-        while($row = $query->fetch(PDO::FETCH_OBJ)) {
-         echo '<li><b>'.$row->number.'</b></li>';
-        }
-        echo '</ul>';
-
-        $query = $pdo->query('SELECT * FROM `data` ORDER BY `id` DESC');
-        while($row = $query->fetch(PDO::FETCH_OBJ)) {
-         echo '<li><b>'.$row->data.'</b></li>';
-        }
-        echo '</ul>';
-      ?>
-  
+      </form>  
     </div>
-  </body>
+</body>
 </html>
