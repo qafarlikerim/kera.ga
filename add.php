b2b0b1e0228cafb8a47cffbf7a2ef6ffcef81efd
kerim <kerim.qafarli02@gmail.com>
2022-06-01 17:44:58 +0400

    a

--- add.php
+++ add.php
@@ -1,14 +1,20 @@
 <title>Ощибка...</title>
 <a href="https://kera.ga/"><img src="https://cdn.create.vista.com/api/media/medium/470867402/stock-vector-back-left-arrow-square-button?token=" title="вернуться назад..." width="4%"></a>
 <?php
+  $title = $_POST['title'];
+  if($title == '') {
+    echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
+    echo 'Введите имя контакта...';
+    exit();
+  }
 
-require_once 'connect.php';
+  require_once 'connect.php';
 
-$title = $_POST['title'];
-$description = $_POST['description'];
-$price = $_POST['price'];
+  $title = $_POST['title'];
+  $description = $_POST['description'];
+  $price = $_POST['price'];
+  
+  mysqli_query($connect, query:"INSERT INTO `products` (`id`, `title`, `price`, `descripsion`) VALUES (NULL, '$title', '$price', '$description')");
 
-mysqli_query($connect, query:"INSERT INTO `products` (`id`, `title`, `price`, `descripsion`) VALUES (NULL, '$title', '$price', '$description')");
-
-header( string: 'Location: index.php');
+  header('Location: /');
 ?>
