6593295860375650ee202818a984e2752651d117
kerim <kerim.qafarli02@gmail.com>
2022-06-02 14:39:16 +0400

    ф

--- update.php
+++ update.php
@@ -5,7 +5,6 @@
     $people_id = $_GET['id'];
     $people = mysqli_query($connect, query:"SELECT * FROM `people` WHERE `id` = '$people_id'");
     $people = mysqli_fetch_assoc($people);
-    print_r($people);
 ?>
 
 
@@ -19,11 +18,11 @@
     <h3>Update Contact</h3>
     <form action="add.php" method="post">
         <p>Name</p>
-        <input type="text" name="name">
+        <input type="text" name="name" value="<?= $people['name'] ?>">
         <p>Number</p>
-        <input type="number" name="number">
+        <input type="number" name="number" value="<?= $people['name'] ?>">
         <p>Date</p>
-        <input type="date" name="date">
+        <input type="date" name="date" value="<?= $people['date'] ?>">
         <br><br>
         <button type="submit">Add new contact</button>
     </form>
