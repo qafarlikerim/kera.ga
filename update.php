e4cd4866cf5b14148c6d34f17852e7b182a6bbcd
kerim <kerim.qafarli02@gmail.com>
2022-06-02 17:48:44 +0400

    a

--- update.php
+++ update.php
@@ -1,10 +1,7 @@
 <?php
 
-    require_once 'config/connect.php';
+   print_r($_GET);
 
-    $people_id = $_GET['id'];
-    $people = mysqli_query($connect, query:"SELECT * FROM `people` WHERE `id` = '$people_id'");
-    $people = mysqli_fetch_assoc($people);
 ?>
 
 
