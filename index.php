aae94c535e271f8f9ae1f7e6834611c37d56fc77
kerim <kerim.qafarli02@gmail.com>
2022-06-06 15:04:17 +0400

    a

--- index.php
+++ index.php
@@ -14,55 +14,7 @@ require_once 'config/connect.php';
     <link rel="icon" href="/address_book.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<style>
-  hr.new5 {
-    border: 3px solid red;
-    border-radius: 5px;
-  }
-</style>
-<style>
-.button {
-  display: inline-block;
-  padding: 15px 20px;
-  font-size: 8px;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  outline: none;
-  color: #fff;
-  background-color: #4CAF50;
-  border: none;
-  border-radius: 15px;
-  box-shadow: 0 9px #999;
-}
-}
-
-.button:hover {background-color: #3e8e41}
-
-.button:active {
-  background-color: #3e8e41;
-  box-shadow: 0 5px #666;
-  transform: translateY(4px);
-}
-
-.button2 {background-color: #008CBA;} /* Blue */
-.button3 {background-color: #f44336;} /* Red */ 
-</style>
 </head>
-<style>
-    th, td {
-        padding: 10px;
-    }
-
-    th {
-        background: #606060;
-        color: #fff;
-    }
-
-    td {
-        background: #b5b5b5;
-    }
-</style>
 <body>
     <h2 class="red-text" style="color: red;" align="center" ><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
   
