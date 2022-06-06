9e32ca32a65a452a5adb6e953cf669956b8dd628
kerim <kerim.qafarli02@gmail.com>
2022-06-06 14:38:19 +0400

    a

--- index.php
+++ index.php
@@ -101,8 +101,29 @@ require_once 'config/connect.php';
         }
     ?>
 </table>
-  <div class="button-container">
-    <a class="btn btn-default btn-round btn-lg btn-icon" href="vendor/delete.php?id=<?= $people[0] ?>" rel="tooltip" title="delete"><i class="fa fa-instagram"></i></a>
+<div class="profile-page">
+  <div class="wrapper">
+    <div class="page-header page-header-small" filter-color="green">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('images/cc-bg-1.jpg')"></div>
+      <div class="container">
+        <div class="content-center">
+          <div class="cc-profile-image"><a href="https://play.fiba3x3.com/players/7c460d31-8916-450f-9605-c3ffe6647ba0" target="_blank"><img src="images/anthony.jpg" alt="Image"/></a></div>
+          <div class="h2 title">Qafarli Kerim</div>
+          <p class="category text-white">Developer</p><a class="btn btn-primary smooth-scroll mr-2" href="#contact" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Hire Me</a><a class="btn btn-primary" href="#portfolio" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">My Projects</a>
+        </div>
+      </div>
+      <div class="section">
+        <div class="container">
+          <div class="button-container">
+            <a class="btn btn-default btn-round btn-lg btn-icon" href="https://github.com/qafarlikerim" target="_blank" rel="tooltip" title="Follow me on GitHub"><i class="fa fa-github"></i></a>
+            <a class="btn btn-default btn-round btn-lg btn-icon" href="https://www.facebook.com/kerim.qafarli02" target="_blank" rel="tooltip" title="Follow me on Facebook"><i class="fa fa-facebook"></i></a>
+            <a class="btn btn-default btn-round btn-lg btn-icon" href="https://twitter.com/GafarlyK22" target="_blank" rel="tooltip" title="Follow me on Twitter"><i class="fa fa-twitter"></i></a>
+            <a class="btn btn-default btn-round btn-lg btn-icon" href="https://www.instagram.com/kerim_qafarli/" target="_blank" rel="tooltip" title="Follow me on Instagram"><i class="fa fa-instagram"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </body>
 </html>
