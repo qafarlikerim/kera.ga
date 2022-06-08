6917f7d0ae54cc066d92e7684bb7a1c26cc55c1b
kerim <kerim.qafarli02@gmail.com>
2022-06-08 19:18:48 +0400

    j

--- vendor/add.php
+++ vendor/add.php
@@ -2,12 +2,12 @@
 <a href="https://kera.ga/"><img src="https://cdn.create.vista.com/api/media/medium/470867402/stock-vector-back-left-arrow-square-button?token=" title="вернуться назад..." width="4%"></a>
 <?php
 
-  $number = $_POST['number'];
-  if($number == '') {
-    echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
-    echo 'Введите номер контакта...';
-    exit();
-  }
+  // $number = $_POST['number'];
+  // if($number == '') {
+  //   echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
+  //   echo 'Введите номер контакта...';
+  //   exit();
+  // }
 
 
   require_once '../config/connect.php';
