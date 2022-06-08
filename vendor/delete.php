c274657f114262fccc534b024cbc3a191bf680ae
kerim <kerim.qafarli02@gmail.com>
2022-06-08 19:42:36 +0400

    g

--- vendor/delete.php
+++ vendor/delete.php
@@ -8,4 +8,10 @@ mysqli_query($connect, query:"DELETE FROM `people` WHERE `people`.`id` = '$id'")
 
 header('Location: ../index.php');
 
+$id = $_GET['id'];
+
+mysqli_query($connect, query:"DELETE FROM `numbers` WHERE `number`.`id` = '$id'");
+
+header('Location: ../index.php');
+
 ?>
