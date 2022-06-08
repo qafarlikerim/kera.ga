5f7bbfb5808c6aaea7dcebc9a9d3e76ad6ce851a
kerim <kerim.qafarli02@gmail.com>
2022-06-08 19:53:34 +0400

    d

--- vendor/add.php
+++ vendor/add.php
@@ -22,7 +22,7 @@
 
   $number = $_POST['number'];
 
-  mysqli_query($connect, query:"INSERT INTO `numbers` (`id`, `number`) VALUES (NULL, '$number')");
+  mysqli_query($connect, query:"INSERT INTO `numbers` (`id`, `number`, `people_id`) VALUES (NULL, '$number', NULL)");
 
   header('Location: /');
 ?>
--- vendor/delete.php
+++ vendor/delete.php
@@ -6,6 +6,12 @@ $id = $_GET['id'];
 
 mysqli_query($connect, query:"DELETE FROM `people` WHERE `people`.`id` = '$id'");
 
+header('Location: ../index.php');
+
+require_once '../config/connect.php';
+
+$id = $_GET['id'];
+
 mysqli_query($connect, query:"DELETE FROM `numbers` WHERE `numbers`.`id` = '$id'");
 
 header('Location: ../index.php');
