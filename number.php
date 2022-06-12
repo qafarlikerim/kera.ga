5039e86685e8c76f65cd646d7d80982c57c55ff1
kerim <kerim.qafarli02@gmail.com>
2022-06-12 18:01:26 +0400

    a

--- number.php
+++ number.php
@@ -16,14 +16,9 @@ require_once 'config/connect.php';
     <link rel="stylesheet" href="css/style.css">
   </head>
 <body>
-    <h2 class="red-text" style="color: red;" align="center" ><a href="https://qkstudio.ml/">www.QkStudio.ml</a></h2>
-  
-    <hr class="new5">
-
-    <br><br>
     <table width="50%"  align="center">
         <tr>
-            <th>Имя
+            <th>Номер
               <button class="open-button" onclick="openForm()">
                 <img class="img_add" src="img/add.png" alt="добавить контакт">
               </button>
@@ -31,13 +26,13 @@ require_once 'config/connect.php';
         </tr>
 
         <?php
-            $people = mysqli_query($connect, query:"SELECT * FROM `people`");
-            $people = mysqli_fetch_all($people);
+            $numbers = mysqli_query($connect, query:"SELECT * FROM `numbers`");
+            $numbers = mysqli_fetch_all($numbers);
             foreach ($people as $people) {
                 ?>
                     <tr>
-                        <td class="name_range"><?= $people[1] ?></td>
-                        <td class="icon_range"><a href="update.php?id=<?= $people[0] ?>"><img class="table_icon" src="img/edit-icon.png" alt="изменить" title="изменить"></a><a href="vendor/delete.php?id=<?= $people[0] ?>"><img class="table_icon" src="img/delete.png" alt="удалить" title="удалить"></a></td>
+                        <td class="name_range"><?= $numbers[1] ?></td>
+                        <td class="icon_range"><a href="update.php?id=<?= $numbers[0] ?>"><img class="table_icon" src="img/edit-icon.png" alt="изменить" title="изменить"></a><a href="vendor/delete.php?id=<?= $numbers[0] ?>"><img class="table_icon" src="img/delete.png" alt="удалить" title="удалить"></a></td>
                     </tr>
                 <?php
             }
@@ -47,8 +42,8 @@ require_once 'config/connect.php';
     <div class="form-popup" id="myForm">
       <form action="vendor/add.php" class="form-container" method="post">
     
-        <label for="name"><b>Контакт</b></label>
-        <input type="text" placeholder="Введите имя контакта..." name="name" id="name" required>
+        <label for="number"><b>Номер</b></label>
+        <input type="text" placeholder="Введите имя контакта..." name="number" id="number" required>
     
         <button type="submit" name="sendTask" class="btn">Добавить</button>
         <button type="button" name="sendTask" class="btn cancel" onclick="closeForm()">Закрыть</button>
---
+++ vendor/addnumber.php
@@ -0,0 +1,20 @@
+<title>Ощибка...</title>
+<a href="https://kera.ga/"><img src="https://cdn.create.vista.com/api/media/medium/470867402/stock-vector-back-left-arrow-square-button?token=" title="вернуться назад..." width="4%"></a>
+<?php
+
+  $number = $_POST['number'];
+  if($number == '') {
+    echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
+    echo 'Введите номер контакта...';
+    exit();
+  }
+
+
+  require_once '../config/connect.php';
+
+  $number = $_POST['number'];
+
+  mysqli_query($connect, query:"INSERT INTO `numbers` (`id`, `number`) VALUES (NULL, '$number')");
+
+  header('Location: /');
+?>
