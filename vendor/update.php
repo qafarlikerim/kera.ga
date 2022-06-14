c6286126ee40a8d43c81b91d24e0c981488102c1
kerim <kerim.qafarli02@gmail.com>
2022-06-14 12:51:27 +0400

    j

--- vendor/update.php
+++ vendor/update.php
@@ -5,7 +5,7 @@ require_once '../config/connect.php';
 $id = $_POST['id'];
 $number = $_POST['number'];
 
-mysqli_query($connect, query:"UPDATE `people` SET `number` = '$number' WHERE `people`.`id` = '$id'");
+mysqli_query($connect, query:"UPDATE `numbers` SET `number` = '$number' WHERE `numbers`.`id` = '$id'");
 
-header('Location: ../index.php');
+header('Location: ../number.php');
 ?>
