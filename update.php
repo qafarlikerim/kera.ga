07504f125008389310d525e5fdf972aefb8d215b
kerim <kerim.qafarli02@gmail.com>
2022-06-27 16:02:41 +0400

    hdgsjasdga

--- update.php
+++ update.php
@@ -17,22 +17,17 @@ require_once 'config/connect.php';
 </head>
 <body>
     <h3 align="center">Update Number</h3>
-    <table width="50%"  align="center">
-        <tr>
-            <th><p>Number</p></th>
-        </tr>
-        <tr>
-            <td>
-                <div class="form-popup">
-                    <form action="vendor/update.php" class="form-container" method="post">
-                        <input type="hidden" name="id" value="<?= $numbers['people_id'] ?>">
-                
-                        <input type="number" name="number" value="<?= $numbers['number'] ?>">
-                        <button type="submit" class="btn">Update</button>
-                    </form>
-                </div>
-            </td>
-        </tr>
-    </table>
+
+    <div class="form-popup">
+        <form action="vendor/update.php" class="form-container" method="post">
+            <input type="hidden" name="id" value="<?= $numbers['people_id'] ?>">
+            <p>Number</p>
+            <input type="number" name="number" value="<?= $numbers['number'] ?>">
+            <input type="number" name="number" value="<?= $numbers['number'] ?>">
+            <input type="number" name="number" value="<?= $numbers['number'] ?>">
+            <button type="submit" class="btn">Update</button>
+        </form>
+    </div>
+
 </body>
 </html>
