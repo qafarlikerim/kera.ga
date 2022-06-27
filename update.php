770800e57dde8c5c4896a234b79ef19963348436
kerim <kerim.qafarli02@gmail.com>
2022-06-27 15:05:28 +0400

    jhflds

--- update.php
+++ update.php
@@ -17,19 +17,21 @@ require_once 'config/connect.php';
 </head>
 <body>
     <h3 align="center">Update Number</h3>
-    <tr>
+    <table>
     <div class="form-popup">
         <form action="vendor/update.php" class="form-container" method="post">
-            <td>
-            <input type="hidden" name="id" value="<?= $numbers['people_id'] ?>">
-            </td>
-            <td>
-            <p>Number</p>
-            <input type="number" name="number" value="<?= $numbers['number'] ?>">
-            </td>
+            <tr>
+                <th>
+                    <p>Number</p>
+                </th>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="id" value="<?= $numbers['people_id'] ?>"></td>                                    
+                <td><input type="number" name="number" value="<?= $numbers['number'] ?>"></td>                
+            </tr>
             <button type="submit" class="btn">Update</button>
         </form>
     </div>
-    </tr>
+    </table>
 </body>
 </html>
